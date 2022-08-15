88a0c56e90fe436996de8991422ac2aae21f4ea2
Sharad Arora <f20211427@goa.bits-pilani.ac.in>
2022-08-15 18:43:29 +0530

    implement frontend framework

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import Chatpage from "./components/chatroom";
+const ws=new WebSocket('ws://localhost:8080');
 function App() {
+const [username,setUsername]=useState("");
+const [Chat,setChat]=useState(false);
+
+  function handlesubmit(event){
+    event.preventDefault();
+
+  }
+  function user(event){
+    setUsername(event.target.value);
+  }
+  function enableChat(event){
+    event.preventDefault();
+    if(username===""){
+      alert("username cannot be empty");
+      setChat(false);
+    }
+    else{
+      setChat(true);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    !Chat?
+    <div id="loginContainer">
+        <img id="profileImg" src={require('./profile.png')} alt="Profile Avatar"/>
+        <form action="#" onSubmit={handlesubmit} autoComplete="off" id="loginForm"> 
+            <input type="text" placeholder="enter username" name="username" 
+            onChange={user} id="loginInput"></input>
+            <button id="loginButton" type="submit" onClick={enableChat}>login</button>
+        </form>
+    </div>:
+    <Chatpage ws={ws} username={username}/>
   );
 }
 
